75ca1643de7ade90f696e7b945573454d912939f
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2023-05-12 16:49:22 +0100

    D-45: Finding a winner algorithm

--- scripts/game.js
+++ scripts/game.js
@@ -8,6 +8,43 @@ function startNewGame () {
     gameAreaElement.style.display = "block"
 }
 
+function checkForGameOver() {
+    // Checking the rows for equality
+    for (let i=0; i <= 2; i++) {
+        if (gameData[i][0] > 0 && 
+            gameData[i][0] == gameData[i][1] && 
+            gameData[i][1] == gameData[i][2]) {
+            return gameData[i][0]
+        }
+    }
+
+    // Checking the columns for equality
+    for (let i=0; i <= 2; i++) {
+        if (gameData[0][i] > 0 && 
+            gameData[0][i] == gameData[1][i] && 
+            gameData[0][i] == gameData[2][i]) {
+            return gameData[0][i]
+        }
+    }
+
+    // Diagnoal: Top left to bottom right
+    if (gameData[0][0] > 0 &&
+        gameData[0][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[2][2]) {
+            return gameData[0][0]
+    }
+
+
+    // Diagnoal: Bottom left to top right
+    if (gameData[2][0] > 0 &&
+        gameData[2][0] === gameData[1][1] &&
+        gameData[1][1] === gameData[0][2]) {
+            return gameData[2][0]
+    }
+    if (currentRound === 9) return -1
+    return 0;
+}
+
 function switchPlayer() {
     activePlayer = activePlayer === 0 ? 1 : 0
     activePlayerName.textContent = players[activePlayer].name
@@ -28,5 +65,8 @@ function selectGameField(event) {
 
     gameData[selectedRow][selectedColumn] = activePlayer + 1
 
+    const winnerId = checkForGameOver()
+
+    currentRound++
     switchPlayer() 
 }
