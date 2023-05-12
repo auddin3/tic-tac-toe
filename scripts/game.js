526be6a0517e927ac43b2e8da654b042f8a1ecd8
Aysha Uddin <aysha.uddin@kpmg.co.uk>
2023-05-12 15:38:35 +0100

    d-45: Managing turns and field selection

---
+++ scripts/game.js
@@ -0,0 +1,22 @@
+function startNewGame () {
+    if (players[0].name === '' || players[1].name === '') {
+        alert('Please set custom player names for both players!')
+        return;
+    }
+
+    activePlayerName.textContent = players[activePlayer].name
+    gameAreaElement.style.display = "block"
+}
+
+function switchPlayer() {
+    activePlayer = activePlayer === 0 ? 1 : 0
+    activePlayerName.textContent = players[activePlayer].name
+}
+
+function selectGameField(event) {
+    if (event.target.tagName !== 'LI') return
+
+    event.target.textContent = players[activePlayer].symbol
+    event.target.classList.add('disabled')
+    switchPlayer() 
+}
